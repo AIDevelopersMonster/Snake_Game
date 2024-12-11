645714bc52fa8e63a1d1c51e4a96fdb3c093b48c
AIDevelopersMonster <t89257409519@gmail.com>
2024-12-11 22:10:19 +0300

    addr I2C change

--- ARDUINO/UNO/OLED_4Btn/Code/Code.ino
+++ ARDUINO/UNO/OLED_4Btn/Code/Code.ino
@@ -44,7 +44,7 @@ int8_t fruit[2];
 void setup() {
   Serial.begin(9600);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
